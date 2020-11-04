a87f12c6282716f79ba75ded0bdb82ca1d51639a
Chengxu Bian <piensengxv@gmail.com>
2020-11-04 17:36:33 +0800

    add err recovery

---
+++ middlewares/recover.go
@@ -0,0 +1,37 @@
+package middlewares
+
+import (
+	"fmt"
+	"github.com/odysa/amigo/lib"
+	"log"
+	"net/http"
+	"runtime"
+	"strings"
+)
+
+func trace(message string) string {
+	var pcs [32]uintptr
+	n := runtime.Callers(3, pcs[:])
+	var str strings.Builder
+	str.WriteString(message + "\nTraceback:")
+	for _, pc := range pcs[:n] {
+		fn := runtime.FuncForPC(pc)
+		file, line := fn.FileLine(pc)
+		str.WriteString(fmt.Sprintf("\n\t%s:%d", file, line))
+	}
+	return str.String()
+}
+
+func Recovery() lib.HandlerFunc {
+	return func(c *lib.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				message := fmt.Sprintf("%s", err)
+				log.Printf("%s\n\n", trace(message))
+				c.Fail(http.StatusInternalServerError, "Internal error")
+			}
+		}()
+
+		c.Next()
+	}
+}
